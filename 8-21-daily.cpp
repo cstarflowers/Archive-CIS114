20575e3c0cc318814137c90054d340e2d074a766
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2023-08-21 15:43:16 -0400

    Create 8-21-daily.cpp

---
+++ 8-21-daily.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+ // create box variable and set to 37
+ int box;
+ box = 37;
+
+ // create street variable and set to Atlantic
+ string street;
+ street = "Atlantic";
+
+ // create zip variable and set to 41018
+ int zip;
+ zip = 41018;
+
+ // print out school name, Ignite Institute and end line
+ cout << "Ignite Institute" << endl;
+
+ // Add the box name, street name, and the word "Avenue" together to create 37 Atlantic Avenue, then end line
+ cout << box << " " << street << " Avenue" << endl;
+
+ // Print out the zip code with 2 new lines for spaces between this and the final line
+ cout << zip;
+ cout << "\n\n" << endl;
+
+ // Print out attention message as a string
+ cout << "\"Attn Mr. Jerome A. Gels\"" << endl;
+}
