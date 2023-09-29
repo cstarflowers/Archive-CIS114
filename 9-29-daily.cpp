3a4bf8ffcbdf911fcd263362b9a162247a191d78
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2023-09-29 10:26:49 -0400

    Create 9-29-daily.cpp

---
+++ 9-29-daily.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+// NO COMMENTS REQUIRED :3
+
+double payment(int, double, int);
+
+int main() {
+	int loan,
+		duration;
+	double interest,
+		function;
+
+	cout << "How much do you want to borrow?: ";
+	cin >> loan;
+	cout << "What is the current ANNUAL interest rate?(ex. 0.12 = 12%): ";
+	cin >> interest;
+	cout << "What is the duration of the loan?(in months): ";
+	cin >> duration;
+
+	function = payment(loan, interest, duration);
+	cout << "Monthly payment: $" << fixed << setprecision(2) << showpoint << function << endl;
+}
+
+double payment(int l, double i, int d) {
+	double monthlyinterest = i / 12,
+		payment = l * ((monthlyinterest * pow((1 + monthlyinterest), d)) / (pow((1 + monthlyinterest), d) - 1));
+	
+	return payment;
+}
