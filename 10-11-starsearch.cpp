73641d47bfe7606816f05ecc65adc3e909a3895f
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2023-10-13 10:18:45 -0400

    Update 10-11-starsearch.cpp

--- 10-11-starsearch.cpp
+++ 10-11-starsearch.cpp
@@ -5,23 +5,34 @@
 
 using namespace std;
 
-void getJudgeData(int i);
-void calcScore(double& score1, double& score2, double& score3, double& score4, double& score5);
-double findLowest(double& score1, double& score2, double& score3, double& score4, double& score5);
-double findHighest(double& score1, double& score2, double& score3, double& score4, double& score5);
+void getJudgeData(double& i);
+void calcScore(double score1, double score2, double score3, double score4, double score5);
+double findLowest(double score1, double score2, double score3, double score4, double score5);
+double findHighest(double score1, double score2, double score3, double score4, double score5);
 
 double scores[5];
 
 int main() {
 	for (int i = 0; i < 5; i++) {
-		getJudgeData(i);
+		getJudgeData(scores[i]);
 	}
 	calcScore(scores[0], scores[1], scores[2], scores[3], scores[4]);
 }
 
-void getJudgeData(int i) {
-	double score;
+void getJudgeData(double& score) {
+	// While(true) statement with help from Maddox White
+	while (true) {
+		cout << "Please enter scores (0.0 - 10.0): ";
+		cin >> score;
+		if (score >= 0 && score <= 10) {
+			break;
+		}
+		else {
+			cout << "Error: Your number is invalid!" << endl << endl;
+		}
+	}
 
+/*
 	cout << "Please enter scores (0.0 - 10.0): ";
 	cin >> score;
 	if (score >= 0 && score <= 10) {
@@ -32,9 +43,10 @@ void getJudgeData(int i) {
 		cout << "Error: Your number is invalid!" << endl << endl;
 		getJudgeData(i);
 	}
+	*/
 }
 
-void calcScore(double& score1, double& score2, double& score3, double& score4, double& score5) {
+void calcScore(double score1, double score2, double score3, double score4, double score5) {
 	double lowest = findLowest(score1, score2, score3, score4, score5);
 	double highest = findHighest(score1, score2, score3, score4, score5);
 
@@ -45,7 +57,7 @@ void calcScore(double& score1, double& score2, double& score3, double& score4, d
 	cout << fixed << setprecision(1) << showpoint << final_score << ". " << endl;
 }
 
-double findLowest(double& score1, double& score2, double& score3, double& score4, double& score5) {
+double findLowest(double score1, double score2, double score3, double score4, double score5) {
 	double scores_sorted[5] = {
 		score1,
 		score2,
@@ -64,7 +76,7 @@ double findLowest(double& score1, double& score2, double& score3, double& score4
 	return lowest_score;
 }
 
-double findHighest(double& score1, double& score2, double& score3, double& score4, double& score5) {
+double findHighest(double score1, double score2, double score3, double score4, double score5) {
 	double scores_sorted[5] = {
 		score1,
 		score2,
