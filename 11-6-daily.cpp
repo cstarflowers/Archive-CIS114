b224893c0e8731d4669490814abea28d26db1aad
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2023-11-06 10:34:39 -0500

    Create 11-6-daily.cpp

---
+++ 11-6-daily.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+struct movies {
+	string mName;
+	int mYear;
+	double mBoxOffice;
+};
+
+int main() {
+	movies userOne;
+
+	cout << "User One's Movie:" << endl;
+
+	cout << "What's the movie's name?: ";
+	getline(cin, userOne.mName);
+	cout << "What is the movie's year?: ";
+	cin >> userOne.mYear;
+	cout << "What amount did they make in the box office?: ";
+	cin.ignore();
+	cin >> userOne.mBoxOffice;
+
+	cout << endl << "Movie Data:" << endl;
+	cout << "User 1 Movie: " << userOne.mName << endl;
+	cout << "User 1 Year: " << userOne.mYear << endl;
+	cout << "User 1 Box Office: $" << fixed << setprecision(2) << showpoint << userOne.mBoxOffice << endl;
+}
