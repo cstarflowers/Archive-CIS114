2c8b26387a9e9d719eb5dacf7611d351ec9ab7bd
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2023-11-10 10:55:02 -0500

    Update 11-10-class-grades.cpp

--- 11-10-class-grades.cpp
+++ 11-10-class-grades.cpp
@@ -5,41 +5,24 @@
 
 using namespace std;
 // Carson Stary, CIS114
-// Working program, but WIP
 
+void letterGrade(int, int);
 struct grades {
 	string name;
 	double grade;
 	char letter;
 };
 
+const int USERS = 3;
+grades studentGrades[USERS];
 int main() {
-	const int USERS = 3;
-	grades studentGrades[USERS];
-
 	for (int i = 0; i < USERS; i++) {
 		if (i > 0) cin.ignore();
 		cout << "Enter Student number " << i + 1 << "'s name: ";
 		getline(cin, studentGrades[i].name);
 		cout << "Enter Student number " << i + 1 << "'s average: ";
 		cin >> studentGrades[i].grade;
-
-		double grade = studentGrades[i].grade;
-		if (grade >= 90) {
-			studentGrades[i].letter = 'A';
-		}
-		else if (grade >= 80) {
-			studentGrades[i].letter = 'B';
-		}
-		else if (grade >= 70) {
-			studentGrades[i].letter = 'C';
-		}
-		else if (grade >= 60) {
-			studentGrades[i].letter = 'D';
-		}
-		else {
-			studentGrades[i].letter = 'F';
-		}
+		letterGrade(studentGrades[i].grade, i);
 	}
 
 	cout << endl << endl << "CIS 114 Grades" << endl << "*************" << endl;
@@ -47,5 +30,23 @@ int main() {
 		cout << "Student name: " << studentGrades[j].name << endl;
 		cout << "Average: " << fixed << setprecision(1) << showpoint << studentGrades[j].grade << endl;
 		cout << "Grade: " << studentGrades[j].letter << endl << endl;
+	}
+}
+
+void letterGrade(int grade, int i) {
+	if (grade >= 90) {
+		studentGrades[i].letter = 'A';
+	}
+	else if (grade >= 80) {
+		studentGrades[i].letter = 'B';
+	}
+	else if (grade >= 70) {
+		studentGrades[i].letter = 'C';
+	}
+	else if (grade >= 60) {
+		studentGrades[i].letter = 'D';
+	}
+	else {
+		studentGrades[i].letter = 'F';
 	}
 }
