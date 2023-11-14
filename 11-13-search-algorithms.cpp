084ddd33587f5f9daa88964ffa52a909f1971fa6
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2023-11-14 12:31:44 -0500

    Create 11-13-search-algorithms.cpp

---
+++ 11-13-search-algorithms.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// Carson Stary, CIS114
+// Functional -- Work In Progress
+
+int main() {
+	int numbers[] = { 1, 2, 3, 4, 7, 10, 20, 22, 25, 26, 34, 37, 38, 40, 41, 47, 49 },
+		numElements = sizeof(numbers) / sizeof(numbers[0]),
+		guess,
+		middle,
+		left = 0,
+		right = numElements - 1,
+		ctr = 1,
+		i;
+
+	cout << "Enter a number less than 50: ";
+	cin >> guess;
+
+	while (left <= right) {
+		middle = left + (right - left) / 2;
+		if (numbers[middle] == guess) {
+			break;
+		}
+
+		if (numbers[middle] < guess) {
+			left = middle + 1;
+		}
+		else {
+			right = middle - 1;
+		}
+		ctr++;
+	}
+
+	for (i = 0; i < numElements; i++) {
+		if (numbers[i] == guess) {
+			break;
+		}
+	}
+
+	cout << endl << "Number " << guess << " found at element " << i << endl;
+	cout << "It took " << ctr << " comparison(s) using the Binary search algorithm." << endl;
+	ctr = 1;
+
+	for (int j = 0; j < numElements; j++) {
+		if (numbers[j] == guess) {
+			break;
+		}
+		ctr++;
+	}
+
+	cout << endl << "Number " << guess << " found at element " << i << endl;
+	cout << "It took " << ctr << " comparison(s) using the Linear search algorithm." << endl;
+}
